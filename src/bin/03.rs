676e395ee57cf7be07f42d8da3fce454b7667014
Jordan Christensen <thebigjc@gmail.com>
2023-12-03 14:20:20 -0500

    Part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,8 +1,23 @@
 advent_of_code::solution!(3);
 
+use itertools::Itertools;
 use std::collections::HashSet;
 
+type NumPoint = (usize, usize, usize, usize);
+
 pub fn part_one(input: &str) -> Option<u32> {
+    score_parts(input, is_symbol, part_one_score)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    score_parts(input, |c| c == '*', part_two_score)
+}
+
+fn score_parts(
+    input: &str,
+    f_symbol: fn(char) -> bool,
+    scorer: fn(parts: HashSet<NumPoint>, input: String) -> u32,
+) -> Option<u32> {
     let line_len = input.lines().next()?.len();
     let input = input.replace('\n', "");
     let chars = input.chars().filter(|c| *c != '\n').collect::<Vec<char>>();
@@ -10,7 +25,7 @@ pub fn part_one(input: &str) -> Option<u32> {
     let mut parts = HashSet::new();
 
     for (i, c) in chars.iter().enumerate() {
-        if is_symbol(*c) {
+        if f_symbol(*c) {
             let x = i % line_len;
             let y = i / line_len;
             let deltas = [
@@ -30,20 +45,49 @@ pub fn part_one(input: &str) -> Option<u32> {
                     continue;
                 }
                 let coords = find_number(x_i as usize, y_i as usize, line_len, &chars);
-                parts.insert(coords);
+                match coords {
+                    Some((a, b)) => {
+                        parts.insert((x, y, a, b));
+                    }
+                    None => continue,
+                }
             }
         }
     }
 
+    Some(scorer(parts, input))
+}
+
+fn part_one_score(parts: HashSet<NumPoint>, input: String) -> u32 {
     let mut sum = 0;
     for part in parts.iter() {
-        let val = part.map_or(0, |(a, b)| input[a..b].parse::<u32>().unwrap());
-        println!("{}, {:?}", val, part);
-
-        sum += val;
+        let (_x, _y, a, b) = part;
+        sum += input[*a..*b].parse::<u32>().unwrap();
     }
+    sum
+}
 
-    Some(sum)
+fn part_two_score(parts: HashSet<NumPoint>, input: String) -> u32 {
+    let mut parts_vec = parts.iter().collect::<Vec<&NumPoint>>();
+    parts_vec.sort();
+    parts_vec
+        .iter()
+        .group_by(|(x, y, _a, _b)| (x, y))
+        .into_iter()
+        .map(|(_k, v)| {
+            let mut product = 1;
+            let v = v.collect::<Vec<&&NumPoint>>();
+            if v.len() == 1 {
+                return 0;
+            }
+
+            for (_x, _y, a, b) in v {
+                product *= input[*a..*b].parse::<u32>().unwrap();
+            }
+
+            product
+        })
+        .sum()
 }
 
 fn find_number(x: usize, y: usize, line_len: usize, chars: &[char]) -> Option<(usize, usize)> {
@@ -77,10 +121,6 @@ fn is_symbol(c: char) -> bool {
     !c.is_ascii_digit() && c != '.'
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -94,6 +134,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
